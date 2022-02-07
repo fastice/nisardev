fixed units, added save to and restore from netcdf

nisardev/cvPoints.py
@@ -79,6 +79,15 @@ class cvPoints:
         '''
         self.cvFile = cvFile
 
+    def _checkUnits(self, units):
+        '''
+        Check units return True for valid units. Print message for invalid.
+        '''
+        if units not in ['m', 'km']:
+            print('Invalid units: must be m or km')
+            return False
+        return True
+
     def checkCVFile(self):
         '''
         Check cvfile exists.
@@ -163,7 +172,8 @@ class cvPoints:
         #
         self.setSRS()
         self.nocull = np.ones(self.vx.shape, dtype=bool)  # set all to no cull
-        self.x, self.y = self.lltoxym(self.lat, self.lon)  # to xy coords
+        # to xy coords - x, y in meters internally
+        self.x, self.y = self.lltoxy(self.lat, self.lon, units='m')  
 
     def writeCVs(self, cvFileOut, fp=None, comment=None, keepOpen=False):
         '''
@@ -274,21 +284,21 @@ class cvPoints:
     def _cvVels(func):
         ''' decorator to interpolate cv values from vel map '''
         @functools.wraps(func)
-        def cvV(*args, date=None, **kwargs):
+        def cvV(*args, units='m', date=None, **kwargs):
             x, y = func(*args)
-            result = args[1].interp(x, y, date=date, **kwargs)
+            result = args[1].interp(x, y, units=units, date=date, **kwargs)
             return result
         return cvV
 
     @_cvVels
-    def vRangeData(self, vel, minv, maxv, date=None):
+    def vRangeData(self, vel, minv, maxv, units='m', date=None):
         ''' Get velocity from vel map for points in range (vmin,vmax).'''
-        return self.xyVRangem(minv, maxv)
+        return self.xyVRange(minv, maxv)
 
     @_cvVels
-    def vAllData(self, vel, date=None):
+    def vAllData(self, vel, units='m', date=None):
         ''' Get velocity from vel map for all points.'''
-        return self.xyAllm()
+        return self.xyAll()
 
     #
     # ===================== Stats Stuff ============================
@@ -323,6 +333,9 @@ class cvPoints:
 
         @functools.wraps(func)
         def mstd(*args, table=False, date=None, **kwargs):
+            if 'units' in kwargs:
+                print('units is not an option for this function')
+                return None
             x, y, iPts, midDate = func(*args, date=date)
             dvx, dvy = args[0].cvDifferences(x, y, iPts, args[1], date=date)
             iGood = np.isfinite(dvx)
@@ -342,7 +355,7 @@ class cvPoints:
     @_stats
     def vRangeStats(self, vel, minv, maxv, date=None):
         ''' get stats for cvpoints in range (minv,maxv) '''
-        x, y = self.xyVRangem(minv, maxv)
+        x, y = self.xyVRange(minv, maxv, units='m')
         iPts = self.vRangeCVs(minv, maxv)
         if date is None:
             dateReturn = vel.midDate.strftime('%Y-%m-%d')
@@ -404,7 +417,7 @@ class cvPoints:
     # ===================== Coordinate Stuff ============================
     #
 
-    def lltoxym(self, lat, lon):
+    def lltoxy(self, lat, lon, units='m'):
         '''
         Convert lat lon (deg) to x y (m)
         Parameters
@@ -420,8 +433,13 @@ class cvPoints:
         y : nparray
             y coordinate in m..
         '''
+        if not self._checkUnits(units):
+            return None, None
+        #
         if self.xyproj is not None:
             x, y = self.lltoxyXform.transform(lat, lon)
+            if units == 'km':
+                return self._toKM(x, y)
             return x, y
         else:
             myError("lltoxy: proj not defined")
@@ -437,7 +455,10 @@ class cvPoints:
         iZero = self.zeroCVs()
         return self.lat(iZero), self.lat(iZero)
 
-    def xyZerom(self):
+    def _toKM(self, x, y):
+        return x/1000., y/1000.
+
+    def xyZero(self, units='m'):
         '''
         Return x and y (m) coordinates of zero CVs.
         Returns
@@ -445,21 +466,15 @@ class cvPoints:
         x,y  : nparray
            x and y in m of zero CVs.
         '''
+        if not self._checkUnits(units):
+            return None, None
+        # find zero points
         iZero = self.zeroCVs()
+        if units == 'km':
+            return self._tokm(self.x[iZero], self.y[iZero])
         return self.x[iZero], self.y[iZero]
 
-    def xyZerokm(self):
-        '''
-        Return x and y (km) coordinates of zero CVs.
-        Returns
-        -------
-        x,y  : nparray
-           x and y in km of zero CVs.
-        '''
-        x, y = self.xyZerom()
-        return x/1000., y/1000.
-
-    def xyAllm(self):
+    def xyAll(self, units='m'):
         '''
         Return x and y (m) coordinates of all CVs.
         Returns
@@ -467,19 +482,14 @@ class cvPoints:
         x,y  : nparray
            x and y in m of all CVs.
         '''
+        if not self._checkUnits(units):
+            return None, None
+        #
+        if units == 'km':
+            return self._toKM(self.x, self.y)
         return self.x, self.y
 
-    def xyAllkm(self):
-        '''
-        Return x and y (km) coordinates of all CVs.
-        Returns
-        -------
-        x,y  : nparray
-           x and y in km of all CVs.
-        '''
-        return self.x/1000., self.y/1000.
-
-    def xyVRangem(self, minv, maxv):
+    def xyVRange(self, minv, maxv, units='m'):
         '''
         Return x and y (m) coordinates for pts with speed in range (minv,maxv).
         Returns
@@ -487,28 +497,20 @@ class cvPoints:
         x,y  : nparray
            x and y in m of all CVs.
         '''
+        if not self._checkUnits(units):
+            return None, None
+        # find points and return
         iRange = self.vRangeCVs(minv, maxv)
+        if units == 'km':
+            return self._toKM(self.x[iRange], self.y[iRange])
         return self.x[iRange], self.y[iRange]
-
-    def xyVRangekm(self, minv, maxv):
-        '''
-        Return x and y (km) coords for pts with speed in range (minv,maxv).
-        Returns
-        -------
-        x,y  : nparray
-           x and y in km of all CVs.
-        '''
-        x, y = self.xyVRangem(minv, maxv)
-        return x/1000., y/1000.
-    #
-    # ===================== Plot Cv locations Stuff ========================
-    #
 
     def _plotCVLocs(func):
         ''' Decorator for plotting locations in range (vmin,vmax). '''
         @functools.wraps(func)
-        def plotCVXY(*args, vel=None, ax=None, nSig=3, date=None, **kwargs):
-            x, y = func(*args, nSig=nSig)
+        def plotCVXY(*args, vel=None, ax=None, nSig=3, units='m', date=None,
+                     **kwargs):
+            x, y = func(*args, units=units, nSig=nSig)
             #
             if vel is not None:
                 result = vel.interp(x, y, date=date)
@@ -520,13 +522,13 @@ class cvPoints:
                 if keyw not in kwargs:
                     kwargs[keyw] = value
             if ax is None:
-                plt.plot(x*0.001, y*0.001, **kwargs)
+                plt.plot(x, y, **kwargs)
             else:
-                ax.plot(x*0.001, y*0.001,  **kwargs)
+                ax.plot(x, y,  **kwargs)
         return plotCVXY
 
     @_plotCVLocs
-    def plotVRangeCVLocs(self, minv, maxv, date=None, **kwargs):
+    def plotVRangeCVLocs(self, minv, maxv, units='m', date=None, **kwargs):
         '''
         plot x,y locations for points where maxv > v > min.
         Parameters
@@ -540,10 +542,12 @@ class cvPoints:
         None
             DESCRIPTION.
         '''
-        return self.xyVRangem(minv, maxv)
+        if not self._checkUnits(units):
+            return None, None
+        return self.xyVRange(minv, maxv, units=units)
 
     @_plotCVLocs
-    def plotAllCVLocs(self, date=None, **kwargs):
+    def plotAllCVLocs(self, units='m', date=None, **kwargs):
         '''
         plot x,y locations for points where maxv > v > min.
         Parameters
@@ -554,9 +558,11 @@ class cvPoints:
         None
             DESCRIPTION.
         '''
-        return self.xyAllm()
+        if not self._checkUnits(units):
+            return None, None
+        return self.xyAll(units=units)
 
-    def getOutlierLocs(self, minv, maxv, vel, nSig=3):
+    def getOutlierLocs(self, minv, maxv, vel, units='m', nSig=3):
         ''' Get outliers where difference in either velocity component is >
         nSig*sigma for points in range (minv,maxv)
         Parameters
@@ -572,9 +578,12 @@ class cvPoints:
         None
             DESCRIPTION.
         '''
-        x, y = self.xyVRangem(minv, maxv)  # get points in range
-        iPts = self.vRangeCVs(minv, maxv)  # Just points in range (minv,vmaxv)
-        dvx, dvy = self.cvDifferences(x, y, iPts, vel)  # get diffs
+        if not self._checkUnits(units):
+            return None, None
+        x, y = self.xyVRange(minv, maxv, units=units)  # get points in range
+        # Just points in range (minv,vmaxv)
+        iPts = self.vRangeCVs(minv, maxv, units=units)
+        dvx, dvy = self.cvDifferences(x, y, iPts, vel, units=units)
         # Compute valid points and reduce all variables to good points
         iGood = np.isfinite(dvx)
         dvx, dvy = dvx[iGood], dvy[iGood]
@@ -587,7 +596,7 @@ class cvPoints:
         return x[iOut], y[iOut]
 
     @_plotCVLocs
-    def plotOutlierLocs(self, minv, maxv, vel, nSig=3):
+    def plotOutlierLocs(self, minv, maxv, vel, units='m', nSig=3):
         ''' Plot outliers where difference in either velocity component is >
         nSig*sigma for points in range (minv,maxv)
         Parameters
@@ -603,7 +612,7 @@ class cvPoints:
         None
             DESCRIPTION.
         '''
-        return self.getOutlierLocs(minv, maxv, vel, nSig=nSig)
+        return self.getOutlierLocs(minv, maxv, vel, units=units, nSig=nSig)
 
     #
     # ===================== CV plot differences ===============================
@@ -625,6 +634,9 @@ class cvPoints:
         @functools.wraps(func)
         def plotp(*args,  ax=None, xColor='r', yColor='b', date=None,
                   legendKwargs={}, **kwargs):
+            if 'units' in kwargs:
+                print('units is not an option for this function')
+                return
             x, y, iPts = func(*args)
             dx, dy = dask.compute(args[0].cvDifferences(x, y, iPts, args[1],
                                                         date=date))[0]
@@ -674,7 +686,7 @@ class cvPoints:
         iPts : bool array
             points.
         '''
-        x, y = self.xyVRangem(minv, maxv)
+        x, y = self.xyVRange(minv, maxv)
         iPts = self.vRangeCVs(minv, maxv)
         return x, y, iPts
 
@@ -702,10 +714,13 @@ class cvPoints:
         @functools.wraps(func)
         def ploth(*args, axes=None, xColor='r', yColor='b', date=None,
                   **kwargs):
+            if 'units' in kwargs:
+                print('units is not an option for this function')
+                return
             x, y, iPts = func(*args)
             dx, dy = dask.compute(args[0].cvDifferences(x, y, iPts, args[1],
                                                         date=date))[0]
-            iGood = np.isfinite(dx)
+            # iGood = np.isfinite(dx)
             dx, dy = clipTail(dx), clipTail(dy)  # Set vals > 3sig to 3sig
             if axes is None:
                 _, axes = plt.subplots(1, 2)
@@ -752,11 +767,11 @@ class cvPoints:
         iPts : bool array
             points.
         '''
-        x, y = self.xyVRangem(minv, maxv)
-        iPts = self.vRangeCVs(minv, maxv, date=date)
+        x, y = self.xyVRange(minv, maxv)
+        iPts = self.vRangeCVs(minv, maxv)
         return x, y, iPts
 
-    def cvDifferences(self, x, y, iPts, vel , date=None):
+    def cvDifferences(self, x, y, iPts, vel, units='m', date=None):
         '''
         Interpolate the velocity components from velocity map at the c/v
         point locations and return the differences (vx_map - vx_cv).
@@ -777,7 +792,7 @@ class cvPoints:
         dvy nparray
             vy difference for good points.
         '''
-        vx, vy, vv = vel.interp(x, y, date=date)
+        vx, vy, vv = vel.interp(x, y, units=units, date=date)
         # subtract cvpoint values args[0] is self
         dvx, dvy = vx - self.vx[iPts], vy - self.vy[iPts]
         # Return valid points and locations where good
@@ -854,7 +869,7 @@ class cvPoints:
         if len(toCull) > 0:
             self.nocull[toCull] = False
 
-    def boundingBox(self, x=None, y=None, pad=10000.):
+    def boundingBox(self, units='m', x=None, y=None, pad=10000.):
         '''
         Compute Bounding box for tiepoints
         Returns
@@ -863,7 +878,7 @@ class cvPoints:
 
         '''
         if x is None or y is None:
-            x, y = self.xyAllm()
+            x, y = self.xyAll(units=units)
         values = np.around([np.min(x) - pad, np.min(y) - pad,
                             np.max(x) + pad, np.max(y) + pad], -2)
         return dict(zip(['minx', 'miny', 'maxx', 'maxy'], values))

nisardev/nisarBase2D.py
@@ -47,7 +47,6 @@ class nisarBase2D():
         self.subset = None
         self.workingXR = None
         self.flipY = True
-        self.url = False
         self.useStackstac = False
         self.xforms = {}  # Cached pyproj transforms
         dask.config.set(num_workers=numWorkers)
@@ -95,7 +94,7 @@ class nisarBase2D():
     # Projection routines
     # ------------------------------------------------------------------------
 
-    def computePixEdgeCornersXYM(self):
+    def computePixEdgeCornersXYM(self, units='m'):
         '''
         Return dictionary with corner locations. Note unlike pixel
         centered xx, x0 etc values, these corners are defined as the outer
@@ -111,8 +110,13 @@ class nisarBase2D():
             myError(f'Geometry param not defined size {self.nx,self.ny}, '
                     'origin {self.x0,self.y0}), or pix size {self.dx,self.dy}')
         # compute pixel corners from pixel centers
+        self.checkUnits(units)
+        #
         xll, yll = self.x0 - self.dx/2, self.y0 - self.dx/2
         xur, yur = xll + self.sx * self.dx, yll + self.sy * self.dy
+        if units == 'km':
+            xll, yll = self._toKM(xll, yll)
+            xur, yur = self._toKM(xur, yur)
         xul, yul = xll, yur
         xlr, ylr = xur, yll
         corners = {'ll': {'x': xll, 'y': yll}, 'lr': {'x': xlr, 'y': ylr},
@@ -191,7 +195,7 @@ class nisarBase2D():
             return func(inst, xout, yout, *args, date=date, **kwargs)
         return convertCoordsInner
 
-    def checkUnits(self, units):
+    def _checkUnits(self, units):
         '''
         Check units return True for valid units. Print message for invalid.
         '''
@@ -219,7 +223,7 @@ class nisarBase2D():
         np float array
             Interpolated valutes from x,y locations.
         '''
-        if not self.checkUnits(units):
+        if not self._checkUnits(units):
             return
         return self._interpNP(x, y, myVars, date=date, units=units, **kwargs)
 
@@ -279,7 +283,6 @@ class nisarBase2D():
         url bool, optional
             Set true if fileNameBase is a link
         '''
-        self.url = url
         # Do a lazy open on the tiffs
         if stackVar is None:
             self.xr = self.lazy_openTiff(fileNameBase, url=url, masked=masked,
@@ -296,9 +299,30 @@ class nisarBase2D():
             self.xr['time1'] = time1
         if time2 is not None:
             self.xr['time2'] = time2
+        #
+        self.xr.rename('None')
         # Save variables
         self._mapVariables()
 
+    def readFromNetCDF(self, cdfFile):
+        ''' Load data from netcdf file '''
+        if '.nc' not in cdfFile:
+            cdfFile = f'{cdfFile}.nc'
+        xDS = xr.open_dataset(cdfFile)
+        # Pull the first variable that is not spatial_ref
+        for var in list(xDS.data_vars.keys()):
+            if var != 'spatial_ref':
+                self.xr = xr.DataArray(xDS[var])
+                break
+        try:
+            self.xr['spatial_ref'] = xDS['spatial_ref']
+        except Exception:
+            print('warning missing spatial_ref')
+        #
+        self.workingXR = self.xr
+        self.parseGeoInfo()
+        self._mapVariables()
+        
     def loadRemote(self):
         ''' Load the current XR, either full array if not subset,
         or the subsetted version '''
@@ -390,7 +414,7 @@ class nisarBase2D():
     def _mapVariables(self):
         ''' Map the xr variables to band variables (e.g., vx, vy) '''
         for myVar in self.workingXR.band.data:
-            myVar = myVar.item()
+            myVar = str(myVar)
             bandData = np.squeeze(self.workingXR.sel(band=myVar).data)
             setattr(self, myVar, bandData)
 
@@ -530,10 +554,25 @@ class nisarBase2D():
     # Plotting and display
     # ----------------------------------------------------------------------
 
+    def _dates(self, date, asString=False):
+        ''' Extract dates for a layer based on current date '''
+        date1 = self.datetime64ToDatetime(
+            self.workingXR.time.sel(time=date, method='nearest').time1.data)
+        date2 = self.datetime64ToDatetime(
+            self.workingXR.time.sel(time=date, method='nearest').time2.data)
+        midDate = self.datetime64ToDatetime(
+            self.workingXR.time.sel(time=date, method='nearest').data)
+        if asString:
+            date1 = date1.strftime('%Y-%m-%d')
+            date2 = date2.strftime('%Y-%m-%d')
+            midDate = midDate.strftime('%Y-%m-%d')
+        return midDate, date1, date2
+
     def displayVar(self, var, date=None, ax=None, plotFontSize=14,
                    labelFontSize=12, titleFontSize=15, axisOff=False,
                    vmin=0, vmax=7000, units='m',
-                   title=None, colorBarLabel='Speed (m/yr)', **kwargs):
+                   title=None, midDate=True, colorBarLabel='Speed (m/yr)',
+                   **kwargs):
         '''
         Use matplotlib to show velocity in a single subplot with a color
         bar. Clip to absolute max set by maxv, though in practives percentile
@@ -551,7 +590,7 @@ class nisarBase2D():
         ax : matplot lib ax
             Axis used for plot.
         '''
-        if not self.checkUnits(units):
+        if not self._checkUnits(units):
             return
         if ax is None:
             sx, sy = self.sizeInPixels()
@@ -566,25 +605,31 @@ class nisarBase2D():
             return
         # Plot map
         displayVar = self.workingXR.sel(band=var)
+        #
         if date is not None:
             if type(date) == str:
                 date = datetime.strptime(date, '%Y-%m-%d')
             displayVar = displayVar.sel(time=date, method='nearest')
-        displayVar = np.squeeze(displayVar)
+        else:  # Single band case, so pull its date
+            date = self.datetime64ToDatetime(displayVar.time[0].data)
+        # Create title from dates
         if title is None:
-            titleDate = self.datetime64ToDatetime(displayVar.time.data)
-            title = titleDate.strftime('%Y-%m-%d')
+            middleDate, date1, date2 = self._dates(date, asString=True)
+            if midDate:
+                title = middleDate
+            else:
+                title = f'{date1} - {date2}'
+        displayVar = np.squeeze(displayVar)
         pos = ax.imshow(displayVar, vmin=vmin, vmax=vmax,
-                        extent=getattr(self, f'extentIn{units.title()}')(),
-                        **kwargs)
+                        extent=self.extent(units=units), **kwargs)
         if axisOff:
             ax.axis('off')
         # labels and such.
         cb = plt.colorbar(pos, cax=cbAx, orientation='vertical', extend='max')
         cb.set_label(colorBarLabel, size=labelFontSize)
         cb.ax.tick_params(labelsize=plotFontSize)
-        ax.set_xlabel('X (km)', size=labelFontSize)
-        ax.set_ylabel('Y (km)', size=labelFontSize)
+        ax.set_xlabel(f'X ({units})', size=labelFontSize)
+        ax.set_ylabel(f'Y ({units})', size=labelFontSize)
         ax.tick_params(axis='x', labelsize=plotFontSize)
         ax.tick_params(axis='y', labelsize=plotFontSize)
         ax.set_title(title, fontsize=titleFontSize)
@@ -606,54 +651,24 @@ class nisarBase2D():
         '''
         return self.sx, self.sy
 
-    def _toKm(func):
-        '''
-        Decorator for unit conversion
-        Parameters
-        ----------
-        func : function
-            DESCRIPTION.
-        Returns
-        -------
-        float
-            Coordinates converted to km.
-        '''
-        @functools.wraps(func)
-        def convertKM(*args):
-            return [x*0.001 for x in func(*args)]
-        return convertKM
+    def _toKM(self, x, y):
+        return x/1000., y/1000.
 
-    def sizeInM(self):
+    def size(self, units='m'):
         ''' Return size in meters '''
+        self._checkUnits(units)
+        if units == 'km':
+            return self._toKM(self.sx*self.dx, self.sy*self.dy)
         return self.sx*self.dx, self.sy*self.dy
 
-    @_toKm
-    def sizeInKm(self):
-        '''
-        Using decorator _toKm convert size to km
-        Returns
-        -------
-        sx, sy: float
-            size in km.
-        '''
-        return self.sizeInM()
-
-    def originInM(self):
+    def origin(self, units='m'):
         ''' Return origin in meters '''
+        self._checkUnits(units)
+        if units == 'km':
+            return self._toKM(self.x0, self.y0)
         return self.x0, self.y0
 
-    @_toKm
-    def originInKm(self):
-        '''
-        Using decorator _toKm convert origin to km
-        Returns
-        -------
-        x0, y0: float
-            origin in km.
-        '''
-        return self.originInM()
-
-    def boundsInM(self):
+    def bounds(self, units='m'):
         '''
         Determine data bounds in meters
         Returns
@@ -667,11 +682,16 @@ class nisarBase2D():
         ymax : float
             max y (upper right) coordinate.
         '''
+        self._checkUnits(units)
         xmax = (self.x0 + (self.sx - 1) * self.dx)
-        ymax = (self.y0 + (self.sy-1)*self.dy)
-        return self.x0, self.y0, xmax, ymax
+        ymax = (self.y0 + (self.sy-1) * self.dy)
+        x0, y0 = self.x0, self.y0
+        if units == 'km':
+            x0, y0 = self._toKM(self.x0, self.y0)
+            xmax, ymax = self._toKM(xmax, ymax)
+        return x0, y0, xmax, ymax
 
-    def extentInM(self):
+    def extent(self, units='m'):
         '''
         Determine extent [xmin, xmax, ymin, ymax]
         Returns
@@ -685,45 +705,11 @@ class nisarBase2D():
         ymax : float
             max y (upper right) coordinate.
         '''
-        bounds = self.boundsInM()
+        self._checkUnits(units)
+        bounds = self.bounds(units=units)
         return bounds[0], bounds[2], bounds[1], bounds[3]
 
-    @_toKm
-    def extentInKm(self):
-        '''
-        Determine extent [xmin, xmax, ymin, ymax]
-        Returns
-        -------
-        xmin : float
-            min x (lower left) coordinate.
-        xmax : float
-            max x (upper right) coordinate.
-        ymin : float
-            min y (lower left) coordinate.
-        ymax : float
-            max y (upper right) coordinate.
-        '''
-        bounds = self.boundsInM()
-        return bounds[0], bounds[2], bounds[1], bounds[3]
-
-    @_toKm
-    def boundsInKm(self):
-        '''
-        Determine data bounds in km using _toKM decorator.
-        Returns
-        -------
-        xmin : float
-            min x (lower left) coordinate.
-        ymin : float
-            min y (lower left) coordinate.
-        xmax : float
-            max x (upper right) coordinate.
-        ymax : float
-            max y (upper right) coordinate.
-        '''
-        return self.boundsInM()
-
-    def pixSizeInM(self):
+    def pixSize(self, units='m'):
         '''
         Return pixel size in m
         Returns
@@ -733,17 +719,29 @@ class nisarBase2D():
         dy : float
             pixel size in y dimension.
         '''
+        self._checkUnits(units)
+        if units == 'km':
+            return self._toKM(self.dx, self.dy)
         return self.dx, self.dy
 
-    @_toKm
-    def pixSizeInKm(self):
-        '''
-        Return pixel size in km
+    def toNetCDF(self, cdfFile):
+        ''' Write existing working xarray to file. If a subset exists it will
+        be the subset, ow it will be the entire data set.
+        Parameters
+        ----------
+        cdfFile : str
+            netcdf file name. Will append .nc. if not present
         Returns
         -------
-        dx : float
-            pixel size in x dimension.
-        dy : float
-            pixel size in y dimension.
+        None.
         '''
-        return self.pixSizeInM()
+        if self.subset is None:
+            print('No subset present - set bbox={"minxx"...}')
+            return
+        if '.nc' not in cdfFile:
+            cdfFile = f'{cdfFile}.nc'
+        if os.path.exists(cdfFile):
+            os.remove(cdfFile)
+        # 
+        self.workingXR.to_netcdf(path=cdfFile)
+        return cdfFile